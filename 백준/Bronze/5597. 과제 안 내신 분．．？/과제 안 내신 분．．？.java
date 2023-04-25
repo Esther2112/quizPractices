6a750d587dfb42e90f3262f0f67c82b7b15cc9dd
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-04-26 08:53:49 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,46 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr28 = new int[28];
-        int[] arr30 = new int[30];
-        int[] arr2 = new int[2];
-        for (int i = 0; i < arr28.length; i++) {
-            arr28[i] = Integer.parseInt(br.readLine());
-        }
-        for (int i = 0; i < arr30.length; i++) {
-            arr30[i] = i + 1;
+        int[] arr = new int[30];
+
+        for (int i = 0; i < 28; i++) {
+            arr[Integer.parseInt(br.readLine()) - 1] = 1;
         }
 
-        for (int i = 0; i < arr30.length; i++) {
-            for(int j = 0; j < arr28.length; j++){
-                if(arr30[i] == arr28[j]){
-                    break;
-                }
-                if(j == arr28.length - 1){
-                    if(arr2[0] == 0) {
-                        arr2[0] = arr30[i];
-                    }else {
-                        arr2[1] = arr30[i];
-                    }
-                }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0){
+                System.out.println(i + 1);
             }
-        }
-
-        if(arr2[0] > arr2[1]){
-            int temp = arr2[0];
-            arr2[0] = arr2[1];
-            arr2[1] = temp;
-        }
-
-        for (int i : arr2) {
-            System.out.println(i);
         }
     }
 }
