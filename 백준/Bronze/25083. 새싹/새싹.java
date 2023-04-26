6f66c72c349187b83c7e9a31c318d248cb04dfd0
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-04-26 11:05:53 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 13960 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         sb.append("         ,r'\"7\n");
